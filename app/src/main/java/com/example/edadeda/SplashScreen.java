dca253f7acc459b6cc2966db4fbc354de275b654
Amina <daminka07@gmail.com>
2022-05-22 19:44:01 +0300

    disign patch

---
+++ app/src/main/java/com/example/edadeda/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.edadeda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceStart){
+        super.onCreate(savedInstanceStart);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
